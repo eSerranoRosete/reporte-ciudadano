1a0954379eede8ede8d94405e0c0aa078cd7dc78
eSerranoRosete <eserranor98@gmail.com>
2025-12-05 11:45:55 -0600

    feedback on error

--- src/components/ReporteForm.tsx
+++ src/components/ReporteForm.tsx
@@ -1,10 +1,11 @@
 import {
-  Accordion,
-  AccordionItem,
-  Alert,
-  Button,
-  Input,
-  Textarea,
+	Accordion,
+	AccordionItem,
+	Alert,
+	addToast,
+	Button,
+	Input,
+	Textarea,
 } from "@heroui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CheckCircleIcon } from "@phosphor-icons/react";
@@ -19,295 +20,301 @@ import { AccordionTitle } from "./AccordionTitle";
 import { MapComponent } from "./MapComponent";
 
 const formSchema = z.object({
-  location: z.object({
-    lat: z.number(),
-    lng: z.number(),
-  }),
-  images: z.array(z.string()).min(1),
-  comments: z.string().min(3),
-  reporter: z.object({
-    name: z.string().min(3),
-    email: z.email(),
-    phone: z.string(),
-  }),
+	location: z.object({
+		lat: z.number(),
+		lng: z.number(),
+	}),
+	images: z.array(z.string()).min(1),
+	comments: z.string().min(3),
+	reporter: z.object({
+		name: z.string().min(3),
+		email: z.email(),
+		phone: z.string(),
+	}),
 });
 
 interface ReporteFormProps {
-  title: string;
-  reporteId: string;
+	title: string;
+	reporteId: string;
 }
 
 export function ReporteForm({ title, reporteId }: ReporteFormProps) {
-  const credentials = useCredentials();
+	const credentials = useCredentials();
 
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      images: [],
-    },
-  });
+	const form = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			images: [],
+		},
+	});
 
-  const [count, { increment, set }] = useCounter(1, {
-    min: 1,
-    max: 4,
-  });
+	const [count, { increment, set }] = useCounter(1, {
+		min: 1,
+		max: 4,
+	});
 
-  const submit = useMutation({
-    mutationFn: async (data: z.infer<typeof formSchema>) => {
-      const res = await apiRequest(
-        "/App_Agua/Reporte_Ciudadano_Alta",
-        {
-          apI_Key: credentials.apI_Key,
-          telefono_Denunciante: data.reporter.phone,
-          id_Reporte_Ciudadano: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-          id_Tipo_Reporte_Ciudadano_Agua: reporteId,
-          comentarios: data.comments,
-          ubicacion: "Sin Datos",
-          latitud: data.location.lat,
-          longitud: data.location.lng,
-          precision: 0,
-          nombre_Denunciante: data.reporter.name,
-          correo_Denunciante: data.reporter.email,
-          imagen: data.images[0],
-        },
-        credentials.token,
-        "client",
-      );
-      if (!res.ok) {
-        console.error(res.statusText);
-        return;
-      }
+	const submit = useMutation({
+		mutationFn: async (data: z.infer<typeof formSchema>) => {
+			const res = await apiRequest(
+				"/App_Agua/Reporte_Ciudadano_Alta",
+				{
+					apI_Key: credentials.apI_Key,
+					telefono_Denunciante: data.reporter.phone,
+					id_Reporte_Ciudadano: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+					id_Tipo_Reporte_Ciudadano_Agua: reporteId,
+					comentarios: data.comments,
+					ubicacion: "Sin Datos",
+					latitud: data.location.lat,
+					longitud: data.location.lng,
+					precision: 0,
+					nombre_Denunciante: data.reporter.name,
+					correo_Denunciante: data.reporter.email,
+					imagen: data.images[0],
+				},
+				credentials.token,
+				"client",
+			);
+			if (!res.ok) {
+				throw new Error("Fallo en alta de reporte");
+			}
 
-      const resData = await res.json();
-      console.log("✅", resData);
-      return resData;
-    },
-  });
+			const resData = await res.json();
+			return resData;
+		},
+		onError: () => {
+			addToast({
+				color: "danger",
+				variant: "solid",
+				title: "Error",
+				description: "Ocurrio un error al levantar el reporte",
+			});
+		},
+	});
 
-  const images = form.watch("images");
-  const comments = form.watch("comments");
-  const location = form.watch("location");
-  const reporter = form.watch("reporter");
+	const images = form.watch("images");
+	const comments = form.watch("comments");
+	const location = form.watch("location");
+	const reporter = form.watch("reporter");
 
-  if (submit.isSuccess) {
-    return (
-      <div className="w-full h-full grow bg-background flex items-center justify-center">
-        <div className="text-center flex flex-col items-center">
-          <CheckCircleIcon
-            className="text-[#971438] size-32"
-            weight="duotone"
-          />
-          <h1 className="text-3xl font-medium">¡Gracias!</h1>
-          <p>Tu reporte ha sido enviado.</p>
-          <Button
-            as={Link}
-            to="/"
-            className="w-full mt-10 bg-[#971438] text-white"
-            onPress={() => {
-              submit.reset();
-            }}
-          >
-            Volver al inicio
-          </Button>
-        </div>
-      </div>
-    );
-  }
+	if (submit.isSuccess) {
+		return (
+			<div className="w-full h-full grow bg-background flex items-center justify-center">
+				<div className="text-center flex flex-col items-center">
+					<CheckCircleIcon
+						className="text-[#971438] size-32"
+						weight="duotone"
+					/>
+					<h1 className="text-3xl font-medium">¡Gracias!</h1>
+					<p>Tu reporte ha sido enviado.</p>
+					<Button
+						as={Link}
+						to="/"
+						className="w-full mt-10 bg-[#971438] text-white"
+						onPress={() => {
+							submit.reset();
+						}}
+					>
+						Volver al inicio
+					</Button>
+				</div>
+			</div>
+		);
+	}
 
-  return (
-    <div className="flex flex-col gap-2 grow">
-      <h1 className="text-2xl font-semibold text-center">{title}</h1>
-      <p className="text-default-500 text-sm text-center mb-3">
-        Completa el formulario para levantar un reporte.
-      </p>
-      <Accordion
-        as="form"
-        onSubmit={form.handleSubmit((v) => submit.mutate(v))}
-        variant="splitted"
-        selectedKeys={[String(count)]}
-      >
-        <AccordionItem
-          key={1}
-          onPress={() => set(1)}
-          aria-label="Ubicacion"
-          title={
-            <AccordionTitle
-              title="1. Agrega la ubicacion"
-              isCompleted={!!location}
-            />
-          }
-        >
-          <Alert
-            description="Da click en la ubicacion aproximada"
-            color="primary"
-            className="mb-2"
-          />
-          <div className="w-full aspect-video bg-default-200 rounded-xl">
-            <MapComponent
-              pinCoords={form.getValues("location")}
-              setPinCoords={(coords) => {
-                coords &&
-                  form.setValue("location", coords, { shouldValidate: true });
-              }}
-            />
-          </div>
-          <Button
-            className="w-full mt-2 bg-[#971438] text-white"
-            onPress={increment}
-            type="button"
-          >
-            Siguiente
-          </Button>
-        </AccordionItem>
-        <AccordionItem
-          key={2}
-          onPress={() => set(2)}
-          aria-label="Imagenes"
-          title={
-            <AccordionTitle
-              title="2. Agrega tus imagenes"
-              isCompleted={count > 2}
-            />
-          }
-        >
-          <div className="border border-divider rounded-2xl border-dashed p-4 flex items-center justify-center flex-col gap-2">
-            {!images.length ? (
-              <>
-                <Input
-                  type="file"
-                  multiple
-                  onChange={async (e) => {
-                    if (!e.target.files) return;
+	return (
+		<div className="flex flex-col gap-2 grow">
+			<h1 className="text-2xl font-semibold text-center">{title}</h1>
+			<p className="text-default-500 text-sm text-center mb-3">
+				Completa el formulario para levantar un reporte.
+			</p>
+			<Accordion
+				as="form"
+				onSubmit={form.handleSubmit((v) => submit.mutate(v))}
+				variant="splitted"
+				selectedKeys={[String(count)]}
+			>
+				<AccordionItem
+					key={1}
+					onPress={() => set(1)}
+					aria-label="Ubicacion"
+					title={
+						<AccordionTitle
+							title="1. Agrega la ubicacion"
+							isCompleted={!!location}
+						/>
+					}
+				>
+					<Alert
+						description="Da click en la ubicacion aproximada"
+						color="primary"
+						className="mb-2"
+					/>
+					<div className="w-full aspect-video bg-default-200 rounded-xl">
+						<MapComponent
+							pinCoords={form.getValues("location")}
+							setPinCoords={(coords) => {
+								coords &&
+									form.setValue("location", coords, { shouldValidate: true });
+							}}
+						/>
+					</div>
+					<Button
+						className="w-full mt-2 bg-[#971438] text-white"
+						onPress={increment}
+						type="button"
+					>
+						Siguiente
+					</Button>
+				</AccordionItem>
+				<AccordionItem
+					key={2}
+					onPress={() => set(2)}
+					aria-label="Imagenes"
+					title={
+						<AccordionTitle
+							title="2. Agrega tus imagenes"
+							isCompleted={count > 2}
+						/>
+					}
+				>
+					<div className="border border-divider rounded-2xl border-dashed p-4 flex items-center justify-center flex-col gap-2">
+						{!images.length ? (
+							<>
+								<Input
+									type="file"
+									multiple
+									onChange={async (e) => {
+										if (!e.target.files) return;
 
-                    const files = Array.from(e.target.files);
+										const files = Array.from(e.target.files);
 
-                    const base64Files = await Promise.all(
-                      files.map((file) => {
-                        return new Promise((resolve, reject) => {
-                          const reader = new FileReader();
-                          reader.readAsDataURL(file);
-                          reader.onload = () => resolve(reader.result);
-                          reader.onerror = reject;
-                        });
-                      }),
-                    );
+										const base64Files = await Promise.all(
+											files.map((file) => {
+												return new Promise((resolve, reject) => {
+													const reader = new FileReader();
+													reader.readAsDataURL(file);
+													reader.onload = () => resolve(reader.result);
+													reader.onerror = reject;
+												});
+											}),
+										);
 
-                    form.setValue("images", base64Files as string[], {
-                      shouldValidate: true,
-                    });
-                  }}
-                  label="Seleccionar Imagenes"
-                  accept="image/*"
-                />
-              </>
-            ) : (
-              <>
-                {images.map((image, index) => (
-                  <img key={index} src={image} />
-                ))}
-              </>
-            )}
-          </div>
-          <Button
-            type="button"
-            className="w-full mt-2 bg-[#971438] text-white"
-            onPress={increment}
-          >
-            Siguiente
-          </Button>
-        </AccordionItem>
-        <AccordionItem
-          key={3}
-          onPress={() => set(3)}
-          aria-label="Comentarios"
-          title={
-            <AccordionTitle
-              title="3. Agrega tus comentarios"
-              isCompleted={!!comments}
-            />
-          }
-        >
-          <Controller
-            control={form.control}
-            name="comments"
-            render={({ field }) => (
-              <Textarea
-                {...field}
-                placeholder="Ejemplo: El bache esta pegado a la banqueta del lado derecho."
-              />
-            )}
-          />
-          <Button
-            className="w-full mt-2 bg-[#971438] text-white"
-            onPress={increment}
-            type="button"
-          >
-            Siguiente
-          </Button>
-        </AccordionItem>
+										form.setValue("images", base64Files as string[], {
+											shouldValidate: true,
+										});
+									}}
+									label="Seleccionar Imagenes"
+									accept="image/*"
+								/>
+							</>
+						) : (
+							<>
+								{images.map((image, index) => (
+									<img key={index} src={image} />
+								))}
+							</>
+						)}
+					</div>
+					<Button
+						type="button"
+						className="w-full mt-2 bg-[#971438] text-white"
+						onPress={increment}
+					>
+						Siguiente
+					</Button>
+				</AccordionItem>
+				<AccordionItem
+					key={3}
+					onPress={() => set(3)}
+					aria-label="Comentarios"
+					title={
+						<AccordionTitle
+							title="3. Agrega tus comentarios"
+							isCompleted={!!comments}
+						/>
+					}
+				>
+					<Controller
+						control={form.control}
+						name="comments"
+						render={({ field }) => (
+							<Textarea
+								{...field}
+								placeholder="Ejemplo: El bache esta pegado a la banqueta del lado derecho."
+							/>
+						)}
+					/>
+					<Button
+						className="w-full mt-2 bg-[#971438] text-white"
+						onPress={increment}
+						type="button"
+					>
+						Siguiente
+					</Button>
+				</AccordionItem>
 
-        <AccordionItem
-          key={4}
-          onPress={() => set(4)}
-          aria-label="Comentarios"
-          title={
-            <AccordionTitle
-              title="4. Agrega tus datos"
-              isCompleted={!!reporter}
-            />
-          }
-        >
-          <div className="flex flex-col gap-2">
-            <Controller
-              control={form.control}
-              name="reporter.name"
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  isInvalid={!!form.formState.errors.reporter?.name}
-                  errorMessage={form.formState.errors.reporter?.name?.message}
-                  placeholder="Juan Perez"
-                  label="Tu Nombre"
-                />
-              )}
-            />
-            <Controller
-              control={form.control}
-              name="reporter.email"
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  placeholder="ejemplo@gmail.com"
-                  type="email"
-                  isInvalid={!!form.formState.errors.reporter?.email}
-                  errorMessage={form.formState.errors.reporter?.email?.message}
-                  label="Correo Electronico"
-                />
-              )}
-            />
-            <Controller
-              control={form.control}
-              name="reporter.phone"
-              render={({ field }) => (
-                <Input
-                  placeholder="55 5555 5555"
-                  {...field}
-                  isInvalid={!!form.formState.errors.reporter?.phone}
-                  errorMessage={form.formState.errors.reporter?.phone?.message}
-                  label="Telefono"
-                />
-              )}
-            />
-          </div>
-          <Button
-            isLoading={submit.isPending}
-            type="submit"
-            className="w-full mt-2 bg-[#971438] text-white"
-          >
-            Enviar Reporte
-          </Button>
-        </AccordionItem>
-      </Accordion>
-    </div>
-  );
+				<AccordionItem
+					key={4}
+					onPress={() => set(4)}
+					aria-label="Comentarios"
+					title={
+						<AccordionTitle
+							title="4. Agrega tus datos"
+							isCompleted={!!reporter}
+						/>
+					}
+				>
+					<div className="flex flex-col gap-2">
+						<Controller
+							control={form.control}
+							name="reporter.name"
+							render={({ field }) => (
+								<Input
+									{...field}
+									isInvalid={!!form.formState.errors.reporter?.name}
+									errorMessage={form.formState.errors.reporter?.name?.message}
+									placeholder="Juan Perez"
+									label="Tu Nombre"
+								/>
+							)}
+						/>
+						<Controller
+							control={form.control}
+							name="reporter.email"
+							render={({ field }) => (
+								<Input
+									{...field}
+									placeholder="ejemplo@gmail.com"
+									type="email"
+									isInvalid={!!form.formState.errors.reporter?.email}
+									errorMessage={form.formState.errors.reporter?.email?.message}
+									label="Correo Electronico"
+								/>
+							)}
+						/>
+						<Controller
+							control={form.control}
+							name="reporter.phone"
+							render={({ field }) => (
+								<Input
+									placeholder="55 5555 5555"
+									{...field}
+									isInvalid={!!form.formState.errors.reporter?.phone}
+									errorMessage={form.formState.errors.reporter?.phone?.message}
+									label="Telefono"
+								/>
+							)}
+						/>
+					</div>
+					<Button
+						isLoading={submit.isPending}
+						type="submit"
+						className="w-full mt-2 bg-[#971438] text-white"
+					>
+						Enviar Reporte
+					</Button>
+				</AccordionItem>
+			</Accordion>
+		</div>
+	);
 }
